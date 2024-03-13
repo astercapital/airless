59227b3e022c0b138e8a46795039fe7ae8dba570
Nelson Pires <np@astercapital.com.br>
2024-03-13 15:08:00 -0300

    adjust tests

--- tests/operator/google/test_storage_batch_write_detect_aggregate_operator.py
+++ tests/operator/google/test_storage_batch_write_detect_aggregate_operator.py
@@ -1,20 +1,20 @@
 import pytest
 from datetime import datetime, timedelta, timezone
 from airless.operator.google.storage import BatchWriteDetectAggregateOperator, ProcessTopic
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 from google.api_core.exceptions import NotFound
 
 
-@pytest.fixture
-def operator():
-    op = BatchWriteDetectAggregateOperator()
-    op.process_and_reset_table = MagicMock()
-    op.send_to_process = MagicMock()
-    op.gcs_hook = MagicMock()
-    op.document_db_folder = 'test_folder'
-    op.partially_processed_tables = []
-    op.tables_last_timestamp_processed = {}
-    return op
+# @pytest.fixture
+# def operator():
+#     op = BatchWriteDetectAggregateOperator()
+#     # op.process_and_reset_table = MagicMock()
+#     op.send_to_process = MagicMock()
+#     op.gcs_hook = MagicMock()
+#     op.document_db_folder = 'test_folder'
+#     op.partially_processed_tables = []
+#     op.tables_last_timestamp_processed = {}
+#     return op
 
 class MockBlob:
     def __init__(self, time_created, time_deleted=None, size=None):
@@ -26,31 +26,36 @@ class MockBlob:
 
 ####### is_processing_required
 # Use case 1: blob was created after the last timestamp and before the deadline
-def test_blob_created_after_last_before_deadline(operator):
+def test_blob_created_after_last_before_deadline():
+    operator = BatchWriteDetectAggregateOperator()
     last_timestamp = datetime.now(timezone.utc) - timedelta(minutes=120)
     within_deadline_blob = MockBlob(datetime.now(timezone.utc) - timedelta(minutes=30))
     assert operator.is_processing_required(within_deadline_blob, last_timestamp, 60)
 
 # Use case 2: blob was created after the last timestamp and after the deadline
-def test_blob_created_after_last_after_deadline(operator):
+def test_blob_created_after_last_after_deadline():
+    operator = BatchWriteDetectAggregateOperator()
     last_timestamp = datetime.now(timezone.utc) - timedelta(minutes=120)
     outside_deadline_blob = MockBlob(datetime.now(timezone.utc) - timedelta(minutes=90))
     assert operator.is_processing_required(outside_deadline_blob, last_timestamp, 60)
 
 # Use case 3: blob was created before the last timestamp and before the deadline
-def test_blob_created_before_last_before_deadline(operator):
+def test_blob_created_before_last_before_deadline():
+    operator = BatchWriteDetectAggregateOperator()
     last_timestamp = datetime.now(timezone.utc) - timedelta(minutes=15)
     within_deadline_but_before_last_timestamp_blob = MockBlob(datetime.now(timezone.utc) - timedelta(minutes=30))
     assert not operator.is_processing_required(within_deadline_but_before_last_timestamp_blob, last_timestamp, 60)
 
 # Use case 4: blob was created before the last timestamp and after the deadline
-def test_blob_created_before_last_after_deadline(operator):
+def test_blob_created_before_last_after_deadline():
+    operator = BatchWriteDetectAggregateOperator()
     last_timestamp = datetime.now(timezone.utc) - timedelta(minutes=30)
     outside_deadline_and_before_last_timestamp_blob = MockBlob(datetime.now(timezone.utc) - timedelta(minutes=90))
     assert operator.is_processing_required(outside_deadline_and_before_last_timestamp_blob, last_timestamp, 60)
 
 # Use case 5: blob was deleted
-def test_blob_was_deleted(operator):
+def test_blob_was_deleted():
+    operator = BatchWriteDetectAggregateOperator()
     deleted_blob = MockBlob(datetime.now(timezone.utc) - timedelta(minutes=30), datetime.now(timezone.utc))
     assert not operator.is_processing_required(deleted_blob, datetime.min.replace(tzinfo=timezone.utc), 60)
 
@@ -58,7 +63,8 @@ def test_blob_was_deleted(operator):
 
 ####### update_table_records
 # Use case 1: Updating a table with a new blob when the table is initially empty
-def test_update_table_records_new_blob_empty_table(operator):
+def test_update_table_records_new_blob_empty_table():
+    operator = BatchWriteDetectAggregateOperator()
     blob = MockBlob(datetime.now(timezone.utc), size=100)
     operator.update_table_records(blob, 'test_table', 'file1.txt')
     assert operator.tables['test_table']['size'] == 100
@@ -67,7 +73,8 @@ def test_update_table_records_new_blob_empty_table(operator):
     assert operator.tables['test_table']['max_time_created'] == blob.time_created
 
 # Use case 2: Updating a table with a new blob when the table already has records
-def test_update_table_records_new_blob_existing_table(operator):
+def test_update_table_records_new_blob_existing_table():
+    operator = BatchWriteDetectAggregateOperator()
     initial_blob = MockBlob(datetime.now(timezone.utc) - timedelta(minutes=5), size=100)
     operator.update_table_records(initial_blob, 'test_table', 'initial_file.txt')
     new_blob = MockBlob(datetime.now(timezone.utc), size=200)
@@ -78,7 +85,8 @@ def test_update_table_records_new_blob_existing_table(operator):
     assert operator.tables['test_table']['max_time_created'] == new_blob.time_created
 
 # Use case 3: Correctly updating the min and max time created for blobs
-def test_update_table_records_min_max_time(operator):
+def test_update_table_records_min_max_time():
+    operator = BatchWriteDetectAggregateOperator()
     old_blob = MockBlob(datetime.now(timezone.utc) - timedelta(days=1), size=50)
     operator.update_table_records(old_blob, 'test_table', 'old_file.txt')
     new_blob = MockBlob(datetime.now(timezone.utc), size=150)
@@ -87,7 +95,8 @@ def test_update_table_records_min_max_time(operator):
     assert operator.tables['test_table']['max_time_created'] == new_blob.time_created
 
 # Use case 4: Ensuring size and files list are updated correctly
-def test_update_table_records_size_files_update(operator):
+def test_update_table_records_size_files_update():
+    operator = BatchWriteDetectAggregateOperator()
     blobs = [MockBlob(datetime.now(timezone.utc) - timedelta(minutes=i*10), size=size) for i, size in enumerate([100, 200, 300])]
     filenames = ['file1.txt', 'file2.txt', 'file3.txt']
     total_size = sum(blob.size for blob in blobs)
@@ -100,7 +109,13 @@ def test_update_table_records_size_files_update(operator):
 
 ####### check_and_send_for_processing
 # Use case 1: Table size exceeds the medium size threshold
-def test_check_and_send_size_exceeds_medium(operator):
+@patch('airless.operator.google.storage.BatchWriteDetectAggregateOperator.process_and_reset_table', return_value=MagicMock())
+def test_check_and_send_size_exceeds_medium(mock_process_and_reset_table):
+    operator = BatchWriteDetectAggregateOperator()
+    # operator.document_db_folder = 'test_folder'
+    # operator.partially_processed_tables = []
+    # operator.tables_last_timestamp_processed = {}
+
     table_key = 'test_table'
     operator.tables[table_key] = {'size': 1100, 'files': ['file1.txt', 'file2.txt'], 'min_time_created': datetime.now(timezone.utc), 'max_time_created': datetime.now(timezone.utc)}
     config = {
@@ -108,10 +123,13 @@ def test_check_and_send_size_exceeds_medium(operator):
         'bucket': 'test_bucket'
     }
     operator.check_and_send_for_processing(table_key, config)
-    operator.process_and_reset_table.assert_called_with(table_key, config['bucket'], 'RAW', ProcessTopic.MEDIUM)
+    mock_process_and_reset_table.assert_called_with(table_key, config['bucket'], 'RAW', ProcessTopic.MEDIUM)
 
 # Use case 2: Number of files exceeds the file quantity threshold, size below small size threshold
-def test_check_and_send_files_exceed_quantity_size_below_small(operator):
+@patch('airless.operator.google.storage.BatchWriteDetectAggregateOperator.process_and_reset_table', return_value=MagicMock())
+def test_check_and_send_files_exceed_quantity_size_below_small(mock_process_and_reset_table):
+    operator = BatchWriteDetectAggregateOperator()
+
     table_key = 'test_table'
     operator.tables[table_key] = {'size': 400, 'files': ['file1.txt', 'file2.txt', 'file3.txt', 'file4.txt', 'file5.txt', 'file6.txt'], 'min_time_created': datetime.now(timezone.utc), 'max_time_created': datetime.now(timezone.utc)}
     config = {
@@ -119,10 +137,13 @@ def test_check_and_send_files_exceed_quantity_size_below_small(operator):
         'bucket': 'test_bucket'
     }
     operator.check_and_send_for_processing(table_key, config)
-    operator.process_and_reset_table.assert_called_with(table_key, config['bucket'], config['bucket'], ProcessTopic.SMALL)
+    mock_process_and_reset_table.assert_called_with(table_key, config['bucket'], config['bucket'], ProcessTopic.SMALL)
 
 # Use case 3: Number of files exceeds the file quantity threshold, size above small size threshold
-def test_check_and_send_files_exceed_quantity_size_above_small(operator):
+@patch('airless.operator.google.storage.BatchWriteDetectAggregateOperator.process_and_reset_table', return_value=MagicMock())
+def test_check_and_send_files_exceed_quantity_size_above_small(mock_process_and_reset_table):
+    operator = BatchWriteDetectAggregateOperator()
+
     table_key = 'test_table'
     operator.tables[table_key] = {'size': 600, 'files': ['file1.txt', 'file2.txt', 'file3.txt', 'file4.txt', 'file5.txt', 'file6.txt'], 'min_time_created': datetime.now(timezone.utc), 'max_time_created': datetime.now(timezone.utc)}
     config = {
@@ -130,10 +151,13 @@ def test_check_and_send_files_exceed_quantity_size_above_small(operator):
         'bucket': 'test_bucket'
     }
     operator.check_and_send_for_processing(table_key, config)
-    operator.process_and_reset_table.assert_called_with(table_key, config['bucket'], config['bucket'], ProcessTopic.MEDIUM)
+    mock_process_and_reset_table.assert_called_with(table_key, config['bucket'], config['bucket'], ProcessTopic.MEDIUM)
 
 # Use case 4: Neither size nor number of files exceed their thresholds
-def test_check_and_send_no_threshold_exceeded(operator):
+@patch('airless.operator.google.storage.BatchWriteDetectAggregateOperator.process_and_reset_table', return_value=MagicMock())
+def test_check_and_send_no_threshold_exceeded(mock_process_and_reset_table):
+    operator = BatchWriteDetectAggregateOperator()
+
     table_key = 'test_table'
     operator.tables[table_key] = {'size': 450, 'files': ['file1.txt', 'file2.txt', 'file3.txt'], 'min_time_created': datetime.now(timezone.utc), 'max_time_created': datetime.now(timezone.utc)}
     config = {
@@ -141,13 +165,16 @@ def test_check_and_send_no_threshold_exceeded(operator):
         'bucket': 'test_bucket'
     }
     operator.check_and_send_for_processing(table_key, config)
-    operator.process_and_reset_table.assert_not_called()
+    mock_process_and_reset_table.assert_not_called()
 
 
 
 ####### process_based_on_file_count
 # Use case 1: Single file in directory, size below the small threshold, directory not partially processed
-def test_process_single_file_small_size_not_partially_processed(operator):
+@patch('airless.operator.google.storage.BatchWriteDetectAggregateOperator.send_to_process', return_value=MagicMock())
+def test_process_single_file_small_size_not_partially_processed(mock_send_to_process):
+    operator = BatchWriteDetectAggregateOperator()
+
     directory = 'test_directory'
     data = {
         'files': ['file1.txt'],
@@ -161,10 +188,13 @@ def test_process_single_file_small_size_not_partially_processed(operator):
     time_threshold = datetime.now(timezone.utc) - timedelta(minutes=30)
 
     operator.process_based_on_file_count(directory, data, config, time_threshold)
-    operator.send_to_process.assert_called_once_with('test_bucket', 'RAW', directory, ['file1.txt'], ProcessTopic.SMALL)
+    mock_send_to_process.assert_called_once_with('test_bucket', 'RAW', directory, ['file1.txt'], ProcessTopic.SMALL)
 
 # Use case 2: Multiple files in directory, size below the small threshold, directory not partially processed
-def test_process_multiple_files_small_size_not_partially_processed(operator):
+@patch('airless.operator.google.storage.BatchWriteDetectAggregateOperator.send_to_process', return_value=MagicMock())
+def test_process_multiple_files_small_size_not_partially_processed(mock_send_to_process):
+    operator = BatchWriteDetectAggregateOperator()
+
     directory = 'test_directory'
     data = {
         'files': ['file1.txt', 'file2.txt'],
@@ -178,10 +208,13 @@ def test_process_multiple_files_small_size_not_partially_processed(operator):
     time_threshold = datetime.now(timezone.utc) - timedelta(minutes=30)
 
     operator.process_based_on_file_count(directory, data, config, time_threshold)
-    operator.send_to_process.assert_called_once_with('test_bucket', 'test_bucket', directory, ['file1.txt', 'file2.txt'], ProcessTopic.SMALL)
+    mock_send_to_process.assert_called_once_with('test_bucket', 'test_bucket', directory, ['file1.txt', 'file2.txt'], ProcessTopic.SMALL)
 
 # Use case 3: Directory marked as partially processed, files created before threshold, size above small threshold
-def test_process_partially_processed_directory_above_small_threshold(operator):
+@patch('airless.operator.google.storage.BatchWriteDetectAggregateOperator.send_to_process', return_value=MagicMock())
+def test_process_partially_processed_directory_above_small_threshold(mock_send_to_process):
+    operator = BatchWriteDetectAggregateOperator()
+
     directory = 'test_directory'
     operator.partially_processed_tables.append(directory)
     data = {
@@ -196,10 +229,13 @@ def test_process_partially_processed_directory_above_small_threshold(operator):
     time_threshold = datetime.now(timezone.utc) - timedelta(minutes=30)
 
     operator.process_based_on_file_count(directory, data, config, time_threshold)
-    operator.send_to_process.assert_called_once_with('test_bucket', 'test_bucket', directory, ['file1.txt', 'file2.txt'], ProcessTopic.MEDIUM)
+    mock_send_to_process.assert_called_once_with('test_bucket', 'test_bucket', directory, ['file1.txt', 'file2.txt'], ProcessTopic.MEDIUM)
 
 # Use case 4: Directory marked as partially processed, files created before threshold, size below small threshold
-def test_process_partially_processed_directory_below_small_threshold(operator):
+@patch('airless.operator.google.storage.BatchWriteDetectAggregateOperator.send_to_process', return_value=MagicMock())
+def test_process_partially_processed_directory_below_small_threshold(mock_send_to_process):
+    operator = BatchWriteDetectAggregateOperator()
+
     directory = 'test_directory'
     operator.partially_processed_tables.append(directory)
     data = {
@@ -214,13 +250,15 @@ def test_process_partially_processed_directory_below_small_threshold(operator):
     time_threshold = datetime.now(timezone.utc) - timedelta(minutes=30)
 
     operator.process_based_on_file_count(directory, data, config, time_threshold)
-    operator.send_to_process.assert_called_once_with('test_bucket', 'test_bucket', directory, ['file1.txt', 'file2.txt'], ProcessTopic.SMALL)
+    mock_send_to_process.assert_called_once_with('test_bucket', 'test_bucket', directory, ['file1.txt', 'file2.txt'], ProcessTopic.SMALL)
 
 
 
 ####### get_dataset_and_table_from_filepath
 # Use case 1: Filepath represents a table_name
-def test_get_data_when_filepath_is_a_table(operator):
+def test_get_data_when_filepath_is_a_table():
+    operator = BatchWriteDetectAggregateOperator()
+
     filepath = 'dataset/table_name'
     expected_dataset = 'dataset'
     expected_table = 'table_name'
@@ -231,7 +269,9 @@ def test_get_data_when_filepath_is_a_table(operator):
     assert table == expected_table, "Table name should be extracted correctly from root directory files"
 
 # Use case 2: Filepath represents a file
-def test_get_data_when_filepath_is_a_file(operator):
+def test_get_data_when_filepath_is_a_file():
+    operator = BatchWriteDetectAggregateOperator()
+
     filepath = 'dataset/table_name/filename.file'
     expected_folder = 'dataset/table_name'
     expected_filename = 'filename.file'
@@ -242,7 +282,9 @@ def test_get_data_when_filepath_is_a_file(operator):
     assert filename == expected_filename, "Table name should be extracted correctly from nested directory paths"
 
 # Use case 3: Filepath ends with a slash (indicating a directory)
-def test_get_data_when_filepath_is_a_directory(operator):
+def test_get_data_when_filepath_is_a_directory():
+    operator = BatchWriteDetectAggregateOperator()
+
     filepath = '/dataset/table_name/'
     expected_folder = '/dataset/table_name'
     expected_filename = ''
@@ -256,31 +298,44 @@ def test_get_data_when_filepath_is_a_directory(operator):
 
 ####### verify_table_last_timestamp_processed
 # Use case 1: Timestamp is available in memory
-def test_verify_timestamp_in_memory(operator):
-    directory = 'existing_in_memory_dataset/existing_in_memory_table'
-    expected_timestamp = datetime.now().replace(tzinfo=timezone.utc)
-    operator.tables_last_timestamp_processed[directory] = expected_timestamp
+def test_verify_timestamp_in_memory():
+    operator = BatchWriteDetectAggregateOperator()
 
-    timestamp = operator.verify_table_last_timestamp_processed(directory)
+    with patch.object(operator, 'gcs_hook', MagicMock()):
+        directory = 'existing_in_memory_dataset/existing_in_memory_table'
+        expected_timestamp = datetime.now().replace(tzinfo=timezone.utc)
+        operator.tables_last_timestamp_processed[directory] = expected_timestamp
 
-    assert timestamp == expected_timestamp
+        timestamp = operator.verify_table_last_timestamp_processed(directory)
+
+        assert timestamp == expected_timestamp
 
 # Use case 2: Timestamp is fetched from bucket
-def test_verify_timestamp_fetched_from_bucket(operator):
-    directory = 'existing_in_bucket_dataset/existing_in_bucket_table'
-    expected_timestamp_str = '20230315010101'
-    expected_timestamp_obj = datetime.strptime(expected_timestamp_str, '%Y%m%d%H%M%S').replace(tzinfo=timezone.utc)
-    operator.gcs_hook.read_json.return_value = {'processed_at': expected_timestamp_str}
+# @patch('airless.operator.google.storage.BatchWriteDetectAggregateOperator.gcs_hook', return_value=MagicMock())
+def test_verify_timestamp_fetched_from_bucket():
+    operator = BatchWriteDetectAggregateOperator()
 
-    timestamp = operator.verify_table_last_timestamp_processed(directory)
+    with patch.object(operator, 'gcs_hook', MagicMock()):
+        directory = 'existing_in_bucket_dataset/existing_in_bucket_table'
+        expected_timestamp_str = '20230315010101'
+        expected_timestamp_obj = datetime.strptime(expected_timestamp_str, '%Y%m%d%H%M%S').replace(tzinfo=timezone.utc)
+        operator.gcs_hook.read_json.return_value = {'processed_at': expected_timestamp_str}
 
-    assert timestamp == expected_timestamp_obj
+        timestamp = operator.verify_table_last_timestamp_processed(directory)
+
+        assert timestamp == expected_timestamp_obj
 
 # Use case 3: NotFound exception handled gracefully
-def test_verify_timestamp_not_found_exception(operator):
-    directory = 'missing_directory'
-    operator.gcs_hook.read_json.side_effect = NotFound('Test not found error')
+# @patch('airless.operator.google.storage.BatchWriteDetectAggregateOperator.gcs_hook', return_value=MagicMock())
+def test_verify_timestamp_not_found_exception():
+    operator = BatchWriteDetectAggregateOperator()
 
-    timestamp = operator.verify_table_last_timestamp_processed(directory)
+    with patch.object(operator, 'gcs_hook', MagicMock()):
+        directory = 'missing_directory'
+        operator.gcs_hook.read_json.side_effect = NotFound('Test not found error')
 
-    assert timestamp == datetime.min.replace(tzinfo=timezone.utc)
+        timestamp = operator.verify_table_last_timestamp_processed(directory)
+
+        assert timestamp == datetime.min.replace(tzinfo=timezone.utc)
+
+####### process_files
