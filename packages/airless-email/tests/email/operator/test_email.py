f2bfa7560a64cad9a00d782789f97e51f9787f56
Luiz Yamaoka <luiz.yamaoka@gmail.com>
2025-06-20 18:07:26 -0300

    import operator inside setup

--- packages/airless-email/tests/email/operator/test_email.py
+++ packages/airless-email/tests/email/operator/test_email.py
@@ -3,29 +3,27 @@ import unittest
 
 from unittest.mock import MagicMock, mock_open, patch
 
-from airless.email.operator import GoogleEmailSendOperator
-
-
-os.environ['ENV'] = 'dev'
-os.environ['QUEUE_TOPIC_ERROR'] = 'dev-error'
-os.environ['DEFAULT_RECIPIENT_EMAIL_DOMAIN'] = 'domain.com'
-os.environ['SECRET_SMTP'] = 'fake-smtp'
-
 
 class TestGoogleEmailSendOperatorOperator(unittest.TestCase):
-    def setUp(self):
+    @patch('builtins.open', new_callable=mock_open)
+    def setUp(self, mock_open_file):
+        os.environ['ENV'] = 'dev'
+        os.environ['QUEUE_TOPIC_ERROR'] = 'dev-error'
+        os.environ['DEFAULT_RECIPIENT_EMAIL_DOMAIN'] = 'domain.com'
+        os.environ['SECRET_SMTP'] = 'fake-smtp'
+
+        mock_file_content = '{"user": "test_user", "password": "test_password", "host": "test_host", "port": 587}'
+        mock_file_handle = mock_open_file.return_value
+        mock_file_handle.read.return_value = mock_file_content
+
+        from airless.email.operator import GoogleEmailSendOperator
+
         self.operator = GoogleEmailSendOperator()
         self.operator.queue_hook = MagicMock()
         self.operator.email_hook = MagicMock()
         self.operator.gcs_hook = MagicMock()
 
-    @patch('builtins.open', new_callable=mock_open)
-    def test_recipient_string_to_array(self, mock_open_file):
-        mock_file_content = '{"user": "test_user", "password": "test_password", "host": "test_host", "port": 587}'
-
-        mock_file_handle = mock_open_file.return_value
-        mock_file_handle.read.return_value = mock_file_content
-
+    def test_recipient_string_to_array(self):
         test_data = [
             {'input': 'single', 'expected_output': ['single@domain.com']},
             {
